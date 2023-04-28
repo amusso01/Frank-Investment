ea9aa4544dbcc70a47e364e8cb62a5e4e9eb6698
amusso01 <eandram@hotmail.it>
2023-04-28 11:12:50 +0200

    404

--- 404.php
+++ 404.php
@@ -10,17 +10,18 @@
 get_header(); ?>
 
 
-<main class="main main-error" role="main">
+<main class="main main-error content-block"  role="main">
 
-	<section class="section" id="section-error-page">
+	<section class="section" id="section-error-page" style="padding:150px 0">
 
 		<h1 class="page-title error-title"><?php esc_html_e( 'Content Cannot Be Found', 'foundry' ); ?></h1>
 
 		<div class="page-body error-body">
 
-			<p><?php esc_html_e( 'Unfortunately the content you were looking for could not be found. Please check that the URL is correct or do a search using the form below.', 'foundry' ); ?></p>
+			<p><?php esc_html_e( 'Unfortunately the content you were looking for could not be found. Please check that the URL is correct or go back to homepage with the button below.', 'foundry' ); ?></p>
 
-			<?php get_search_form(); ?>
+			<a href="<?= site_url() ?>"  class="btn">HOME</a>
+
 
 		</div>
 
